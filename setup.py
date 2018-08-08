c25cc03d9e69d51da87a3926871002c2b8e16911
yw-fang <fyuewen@gmail.com>
2018-08-08 10:23:09 +0900

    clone howdoi in 2018 August 8th

---
+++ setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import howdoi
+import os
+
+
+def extra_dependencies():
+    import sys
+    ret = []
+    if sys.version_info < (2, 7):
+        ret.append('argparse')
+    return ret
+
+
+def read(*names):
+    values = dict()
+    extensions = ['.txt', '.rst']
+    for name in names:
+        value = ''
+        for extension in extensions:
+            filename = name + extension
+            if os.path.isfile(filename):
+                value = open(name + extension).read()
+                break
+        values[name] = value
+    return values
+
+long_description = """
+%(README)s
+
+News
+====
+
+%(CHANGES)s
+
+""" % read('README', 'CHANGES')
+
+setup(
+    name='howdoi',
+    version=howdoi.__version__,
+    description='Instant coding answers via the command line',
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Documentation",
+    ],
+    keywords='howdoi help console command line answer',
+    author='Benjamin Gleitzman',
+    author_email='gleitz@mit.edu',
+    maintainer='Benjamin Gleitzman',
+    maintainer_email='gleitz@mit.edu',
+    url='https://github.com/gleitz/howdoi',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'howdoi = howdoi.howdoi:command_line_runner',
+        ]
+    },
+    install_requires=[
+        'pyquery',
+        'pygments',
+        'requests',
+        'requests-cache'
+    ] + extra_dependencies(),
+)
